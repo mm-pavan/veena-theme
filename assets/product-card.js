e7aee2fc2416533559523a2a4e09d8ce49043cef
mm-pavan <pavan.sai@marmeto.com>
2024-02-21 11:04:20 +0530

    Added color filter in main-collection-product-grid.liquid

--- assets/product-card.js
+++ assets/product-card.js
@@ -1,81 +1,132 @@
 
 // Variant selection with variants
-class ProductCard extends HTMLElement {
-  constructor() {
-    super();
-
-    this.productHandle = this.dataset.productHandle;
-    this.sectionId = this.dataset.sectionId;
-    
-    this.variantData = JSON.parse(this.querySelector('script').textContent);
-    this.addEventListener('change', this.onVariantChange);
-  }
-
-  onVariantChange() {
-    this.selectedVariant = this.querySelector('[name="id"]').value;
-    this.getCurrentVariant();
-  }
-
-  getCurrentVariant() {  
-    this.currentVariant = this.variantData.find(variant => variant.id == this.selectedVariant);
-    this.getUpdatedCard();
-  }
-
-  getUpdatedCard() {
-    const url = `/products/${this.productHandle}?variant=${this.currentVariant.id}&section_id=${this.sectionId}`;
-    console.log("section id: ",this.sectionId)
-    fetch(url)
-      .then((response) => response.text())
-      .then((responseText) => {
-        const html = new DOMParser().parseFromString(responseText, "text/html");
-        this.innerHTML = html.querySelector(`[data-product-handle="${this.productHandle}"]`).innerHTML;
-      });
-  }
-}
-
-customElements.define('product-card', ProductCard);
-
-
-// Variant selection with options
 // class ProductCard extends HTMLElement {
 //   constructor() {
 //     super();
 
 //     this.productHandle = this.dataset.productHandle;
 //     this.sectionId = this.dataset.sectionId;
-
-//     this.variantData = JSON.parse(this.querySelector("script").textContent);
-//     this.addEventListener("change", this.onOptionChange);
+    
+//     this.variantData = JSON.parse(this.querySelector('script').textContent);
+//     this.addEventListener('change', this.onVariantChange);
 //   }
 
-//   onOptionChange() {
-//     console.log("started");
-//     this.selectedOptions = Array.from(
-//       this.querySelectorAll('input[type="radio"]:checked'),
-//       (input) => input.value
-//     );
-//     this.currentVariant = this.variantData.find(
-//       (item) =>
-//         JSON.stringify(item.options) == JSON.stringify(this.selectedOptions)
-//     );
+//   onVariantChange() {
+//     this.selectedVariant = this.querySelector('[name="id"]').value;
+//     this.getCurrentVariant();
+//   }
 
+//   getCurrentVariant() {  
+//     this.currentVariant = this.variantData.find(variant => variant.id == this.selectedVariant);
 //     this.getUpdatedCard();
 //   }
 
 //   getUpdatedCard() {
-//     console.log(this.sectionId)
 //     const url = `/products/${this.productHandle}?variant=${this.currentVariant.id}&section_id=${this.sectionId}`;
-
+//     console.log("section id: ",this.sectionId)
 //     fetch(url)
 //       .then((response) => response.text())
 //       .then((responseText) => {
 //         const html = new DOMParser().parseFromString(responseText, "text/html");
-//         this.innerHTML = html.querySelector(
-//           `[data-product-handle="${this.productHandle}"]`
-//         ).innerHTML;
+//         this.innerHTML = html.querySelector(`[data-product-handle="${this.productHandle}"]`).innerHTML;
 //       });
-//     conosle.log("fetched");
 //   }
 // }
 
-// customElements.define("product-card", ProductCard);
+// customElements.define('product-card', ProductCard);
+
+
+Variant selection with options
+class ProductCard extends HTMLElement {
+  constructor() {
+    super();
+
+    this.productHandle = this.dataset.productHandle;
+    this.sectionId = this.dataset.sectionId;
+
+    this.variantData = JSON.parse(this.querySelector("script").textContent);
+    this.addEventListener("change", this.onOptionChange);
+  }
+
+  onOptionChange() {
+    console.log("started");
+    this.selectedOptions = Array.from(
+      this.querySelectorAll('input[type="radio"]:checked'),
+      (input) => input.value
+    );
+    this.currentVariant = this.variantData.find(
+      (item) =>
+        JSON.stringify(item.options) == JSON.stringify(this.selectedOptions)
+    );
+
+    this.getUpdatedCard();
+  }
+
+  getUpdatedCard() {
+    console.log(this.sectionId)
+    const url = `/products/${this.productHandle}?variant=${this.currentVariant.id}&section_id=${this.sectionId}`;
+
+    fetch(url)
+      .then((response) => response.text())
+      .then((responseText) => {
+        const html = new DOMParser().parseFromString(responseText, "text/html");
+        this.innerHTML = html.querySelector(
+          `[data-product-handle="${this.productHandle}"]`
+        ).innerHTML;
+      });
+    conosle.log("fetched");
+  }
+}
+
+customElements.define("product-card", ProductCard);
+
+
+
+// class ProductCard extends HTMLElement {
+//     constructor() {
+//       super();
+  
+//       this.productHandle = this.dataset.productHandle;
+//       this.sectionId = this.dataset.sectionId;
+  
+//       this.variantData = JSON.parse(this.querySelector("script").textContent);
+//       this.addEventListener("mouseover", this.onOptionHover.bind(this));
+//     }
+  
+//     onOptionHover(event) {
+//       const hoveredInput = event.target.closest('input[type="radio"]');
+//       if (!hoveredInput) return;
+  
+//       console.log("started");
+//       this.selectedOptions = Array.from(
+//         this.querySelectorAll('input[type="radio"]:checked'),
+//         (input) => input.value
+//       );
+//       this.selectedOptions[this.selectedOptions.indexOf(hoveredInput.value)] = hoveredInput.value; // Update the hovered input value
+//       this.currentVariant = this.variantData.find(
+//         (item) =>
+//           JSON.stringify(item.options) === JSON.stringify(this.selectedOptions)
+//       );
+  
+//       this.getUpdatedCard();
+//     }
+  
+//     getUpdatedCard() {
+//       console.log(this.sectionId)
+//       const url = `/products/${this.productHandle}?variant=${this.currentVariant.id}&section_id=${this.sectionId}`;
+  
+//       fetch(url)
+//         .then((response) => response.text())
+//         .then((responseText) => {
+//           const html = new DOMParser().parseFromString(responseText, "text/html");
+//           this.innerHTML = html.querySelector(
+//             `[data-product-handle="${this.productHandle}"]`
+//           ).innerHTML;
+//         });
+//       console.log("fetched");
+//     }
+//   }
+  
+//   customElements.define("product-card", ProductCard);
+  
+  
